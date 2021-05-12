cb05d4fdc896bb827a7117968400e3c58a34cce2
Jackson Belizário <jackson.belizario@intelitrader.com.br>
2021-05-12 13:23:46 -0400

    Improvements on subway graph

--- src/views/SubwayMapVisual.tsx
+++ src/views/SubwayMapVisual.tsx
@@ -42,34 +42,27 @@ const SubwayMapVisual = (props: SubwayMapVisualProps) => {
   React.useEffect(() => {
 
     const getSubwayMap = (commits: ICommit[]) => {
-      let _start = 25;
       let _infinityY = Node.height * (commits.length + 1);
       let nodeDict: object | any = {};
-      let nodes: [] = [];
-      let links: [] = [];
-      commits.forEach((c, i) => {
+
+      let nodes = commits.map((c, i) => {
         let node = new Node(c.sha);
-        // Y offset, just increment;
-        node.y = _start;
-        // X is tricky, do it later
-        node.x = _start;
         node.commit = c;
         node.color = Color.parseHex(colors[0]);
         node.secondColor = Color.parseHex(colors[0]);
-        //@ts-ignore
-        nodes.push(node);
         nodeDict[node.commit.sha] = node;
+        return node;
       });
+
       // edge creation
-      commits.forEach((c) => {
+      let links = commits.reduce((acc, c) => {
         if (c.parents.length === 0) {
           let infinityNode = new Node("infty-" + c.sha);
           infinityNode.x = nodeDict[c.sha].x;
           infinityNode.y = _infinityY;
           let newLink = new Link(nodeDict[c.sha], infinityNode);
           newLink.color = nodeDict[c.sha].color;
-          //@ts-ignore
-          links.push(newLink);
+          acc.push(newLink);
         } else {
           c.parents.forEach((p) => {
             if (nodeDict[p]) {
@@ -82,23 +75,20 @@ const SubwayMapVisual = (props: SubwayMapVisualProps) => {
                 newLink.color = nodeDict[c.sha].color;
                 newLink.merge = false;
               }
-              //@ts-ignore
-              links.push(newLink);
+              acc.push(newLink);
             }
           });
         }
-      });
-      let currentMap = new SubwayMap(nodes, links, nodeDict);
-      currentMap = updateMapLayout(currentMap);
-      setGraph(currentMap);
-      setHeight(Math.max(commits.length * Node.height + 10, 55) + "px");
+        return acc;
+      }, []);
+      
+      return startSubWayMap(nodes, links, nodeDict);
     };
 
-    const updateMapLayout = (map: SubwayMap) => {
+    const startSubWayMap = (nodes: Node[], links : any[], nodeDict) => {
+      let map = new SubwayMap(nodes, links, nodeDict);
       let _start = 25;
       let _offset = Node.height;
-      let nodes = map.nodes;
-      let nodeDict = map.nodeDict;
       // New x algorithm, branch lines, closed and open concept
       // let's see, start from top, start a "branch line" and add that commit, mark as open
       // a merge commit comes in, add one parent in it's line, add another to "new branch", mark open
@@ -106,26 +96,15 @@ const SubwayMapVisual = (props: SubwayMapVisualProps) => {
       // any new commits, add to a existing branch if "it's sha is any of existing's parent", if all fail, put it in new branch line
       // a branch line can only close if "a commit with only 1 parent and that parent is already in a branch" comes in
       let branchLines: BranchLine[] = [];
+      
       function placeNodeInNewOrClosed(node: Node): BranchLine {
         let addedToBl = null;
         branchLines.forEach((bl) => {
-          if (!node.processed) {
-            //@ts-ignore
-            let lastCross = !bl.nodes[bl.nodes.length - 1].commit.parents.every(
-              (parent) => {
-                return nodes.indexOf(nodeDict[parent]) > nodes.indexOf(node);
-              }
-            );
-            if (lastCross) {
-              // bl.open = false;
-            }
-
-            if (!bl.open) {
-              addedToBl = bl;
-              bl.nodes.push(node);
-              bl.open = true;
-              node.processed = true;
-            }
+          if (!node.processed && !bl.open) {
+            addedToBl = bl;
+            bl.nodes.push(node);
+            bl.open = true;
+            node.processed = true;
           }
         });
         if (!addedToBl) {
@@ -136,11 +115,11 @@ const SubwayMapVisual = (props: SubwayMapVisualProps) => {
         }
         return addedToBl;
       }
+
       function placeNodeInExisting(node: Node): BranchLine | null {
         let addedToBl = null;
         branchLines.forEach((bl) => {
           if (!node.processed) {
-            //@ts-ignore
             if (bl.nodes[bl.nodes.length - 1].commit.parents[0] === node.commit.sha) {
               // else if a bl's last node is it's parent
               // it's impossible for anything other than the last one to be the parent
@@ -153,57 +132,49 @@ const SubwayMapVisual = (props: SubwayMapVisualProps) => {
         });
         return addedToBl;
       }
-      function processParents(n: Node, bl: BranchLine) {
-        // special case for it's parents, always put the first with itself
-        //@ts-ignore
-        let parent0 = nodeDict[n.commit.parents[0]];
-        let processGrandparent0 = false;
-        if (parent0 && !parent0.processed && bl) {
-          bl.nodes.push(parent0);
-          //@ts-ignore
-          if (nodeDict[n.commit.parents[0]].commit.parents.length > 1) {
-            processGrandparent0 = true;
+
+      // special case for it's parents, always put the first with itself
+      function processParents(sha: string, bl: BranchLine) {
+        let parent = nodeDict[sha];
+        if (parent && !parent.processed) {
+          bl.nodes.push(parent);
+          // process grandparent
+          if (nodeDict[sha].commit.parents.length > 1) {
+            processParents(nodeDict[sha].commit.parents[0], bl);
           }
-          //@ts-ignore
-          nodeDict[n.commit.parents[0]].processed = true;
-        }
-        if (processGrandparent0) {
-          //@ts-ignore
-          processParents(nodeDict[n.commit.parents[0]], bl);
+          nodeDict[sha].processed = true;
         }
       }
+
       nodes.forEach((n, i) => {
         n.y = _start + i * _offset;
-        //@ts-ignore
         let currentSha = n.commit.sha;
-        // if this node is unprocessed
         if (!n.processed) {
-          let addedToBl = null;
-          // see if I can add to an existing branch
-          addedToBl = placeNodeInExisting(n);
-          if (!addedToBl) {
-            addedToBl = placeNodeInNewOrClosed(n); // this method must return a bl
-          }
-          processParents(n, addedToBl);
+          let parentSha = n.commit.parents[0];
+          // Add to an existing branch or new
+          let addedToBl = placeNodeInExisting(n) || placeNodeInNewOrClosed(n);
+          processParents(parentSha, addedToBl);
         }
         // check for closed branch line, make it available for adding
-        //@ts-ignore
         branchLines.forEach((bl) => {
-          //@ts-ignore
           if (bl.nodes[bl.nodes.length - 1].commit.parents.indexOf(currentSha) !== -1) {
             bl.open = false;
           }
         });
       });
+
       // process all branch lines
-      branchLines.forEach((bl, i) => {
-        bl.nodes.forEach((n) => {
+      branchLines = branchLines.map((bl, i) => {
+        bl.nodes = bl.nodes.map((n) => {
           n.x = _start + i * _offset;
           //@ts-ignore
           n.color.setHex(colors[i % colors.length]);
           n.x_order = i;
+          return n;
         });
+        return bl;
       });
+
       map.width = branchLines.length;
       return map;
     };
@@ -211,8 +182,9 @@ const SubwayMapVisual = (props: SubwayMapVisualProps) => {
     // const updateWidth = () => {
     //   setWidth(Math.min(Math.max(graph.width * 50 + 20 + 3, 55), 600));
     // };
-    setGraph(null);
-    getSubwayMap(commits);
+    let graph = getSubwayMap(commits);
+    setGraph(graph);
+    setHeight(Math.max(commits.length * Node.height + 10, 55) + "px");
   }, [commits, setGraph]);
 
   return (
