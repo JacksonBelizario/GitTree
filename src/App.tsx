339ecfc677f7a81685648b65c054af2cf830f259
Jackson Belizário <jacksonluizzb@gmail.com>
2021-05-18 11:38:11 -0400

    Expanded diff view

--- src/App.tsx
+++ src/App.tsx
@@ -2,45 +2,51 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Main from "./views/Main";
-import { Provider } from "redux-zero/react";
-import store from "./store/store";
 import SplitPane from "react-split-pane/lib/SplitPane";
 import Pane from "react-split-pane/lib/Pane";
+import { connect } from 'redux-zero/react';
 
 import "./assets/main.css";
 import "./assets/scss/main.scss";
 import CommitDetail from "./views/CommitDetail";
 import FileViewer from "./views/FileViewer";
 import Footer from "./components/Footer";
+import { IStore } from './store/store';
+import { ISelectedFile } from './utils/interfaces';
+interface StoreProps {
+  selectedFile: ISelectedFile;
+}
 
-const App = () => {
+const mapToProps = (state: IStore): StoreProps => ({
+  selectedFile: state.selectedFile
+});
+
+const App = (props: StoreProps) => {
   return (
-    <Provider store={store}>
-      <div className="bp3-dark flex flex-col h-screen">
-        <Navbar />
-        <div className="main-layout flex-1">
-          <SplitPane split="vertical">
-            <Pane
-              className="sidebar"
-              minSize="210px"
-              defaultSize="250px"
-              maxSize="390px"
-            >
-              <Sidebar />
-            </Pane>
-            <div className="relative h-full">
-              <Main />
-              <FileViewer />
-            </div>
-            <Pane minSize="300px" defaultSize="350px" maxSize="500px">
-              <CommitDetail />
-            </Pane>
-          </SplitPane>
-        </div>
-        <Footer />
+    <div className="bp3-dark flex flex-col h-screen">
+      <Navbar />
+      <div className="main-layout flex-1">
+        <SplitPane split="vertical">
+          <Pane
+            className={`sidebar ${props.selectedFile.path ? "hidden" : ""}`}
+            minSize="210px"
+            defaultSize="210px"
+            maxSize="390px"
+          >
+            <Sidebar />
+          </Pane>
+          <div className="relative h-full">
+            <Main />
+            <FileViewer />
+          </div>
+          <Pane minSize="300px" defaultSize="350px" maxSize="500px">
+            <CommitDetail />
+          </Pane>
+        </SplitPane>
       </div>
-    </Provider>
+      <Footer />
+    </div>
   );
 };
 
-export default App;
+export default connect<IStore>(mapToProps)(App);
